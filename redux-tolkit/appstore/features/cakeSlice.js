867d3df8850c2f5de4075b78570a4d620230e132
saadali <saadali6991550@gmail.com>
2026-03-05 21:30:10 +0500

    extra reducer with builder

--- redux-tolkit/appstore/features/cakeSlice.js
+++ redux-tolkit/appstore/features/cakeSlice.js
@@ -1,5 +1,6 @@
 
 const {createSlice}=  require('@reduxjs/toolkit')
+// for importing other action from other slice for extra reducer const {cakeActions}=require('../features/cakeSlice')
 const initialState= {
     numberofcake:10
 }
@@ -13,7 +14,17 @@ const cakeSlice=createSlice({
         restock:(state,action)=>{
             state.numberofcake += action.payload
         }
-    }
+    },
+    // // if we want two different slice to follow eatchother
+    // extraReducers:{
+    //     ['cake/order']:(state)=>{
+    //         state.numberoficecream-- 
+       extraReducers:(builder)=>{
+        builder.addCase(cakeAction.order,state=>{
+            state.numberoficecream--
+        })
+       } 
+    
 
 })
 
