1cf54c6ae27106791eac311a7705ddb8193fc2bd
saadali <saadali6991550@gmail.com>
2026-03-05 14:46:08 +0500

    7 store of redux(create)

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-console.log('hello saad')
-const cake_order='cake_order'
-function OrderCake(){
-    return{
+const redux = require('redux')
+const createStore = redux.createStore
 
-    type:cake_order,
-    quantity:1
+console.log('hello saad')
+
+const CAKE_ORDER = 'CAKE_ORDER'
+
+function orderCake() {
+  return {
+    type: CAKE_ORDER,
+    quantity: 1
+  }
 }
+
+const initialState = {
+  numberOfCake: 10
 }
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDER:
+      return {
+        numberOfCake: state.numberOfCake - action.quantity
+      }
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+console.log('Initial State:', store.getState())
+ const unsub=  store.subscribe(()=>console.log('updatestate',store.getState()))
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+console.log('Updated State:', store.getState())
+unsub()
+store.dispatch(orderCake())
